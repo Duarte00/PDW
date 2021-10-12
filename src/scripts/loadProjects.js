3c92d51cc447277cab37c14fcadb73b32bc204ab
Daniela <martins.danielamadeira@gmail.com>
2021-10-12 10:18:11 +0100

    cleaned comments

---
+++ src/scripts/loadProjects.js
@@ -0,0 +1,25 @@
+/*
+    Each script should:
+    - Open the corresponding file (BSProjects.txt, MSProjects.txt, etc)
+    - For each line:
+        - Split the contents of the file into a String array 
+        - Load each element in the correct tag
+*/
+
+
+
+function loadBSProjects() {
+
+}
+
+function loadMSProjects() {
+
+}
+
+function loadMSDissertations() {
+
+}
+
+function loadPhDProjects() {
+
+}
